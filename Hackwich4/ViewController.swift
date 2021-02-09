23bf26db5121f0f27f89457c107b6d24fbd87313
esamson6 <77295762+esamson6@users.noreply.github.com>
2021-02-09 11:00:33 -1000

    Class 2/2/2021 - Completed  Parts 1 - 9

--- Hackwich4/ViewController.swift
+++ Hackwich4/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var firstTabLabel: UILabel!
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Pat 7E
+        //when the app first opens up, we want to set the label's text to "About"
+        self.firstTabLabel.text = "About"
     }
 
 
